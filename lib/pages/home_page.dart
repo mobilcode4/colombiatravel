6dada45900dbaaffd26072882e22a0dd6b764eb7
johnsito <jotazo@hotmail.com>
2022-10-28 17:59:50 -0500

    POI - Popayan

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../partials/descripcion.dart';
+import '../partials/tarjeta.dart';
+import '../partials/cabecera.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,30 +11,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String msg =
+      "Departamento: Cauca \nTemperatura: 18 Grados\n\nEs una ciudad del oeste de Colombia, al suroeste de Bogotá. Es conocida por sus edificios coloniales encalados y por ser un centro religioso con procesiones populares en Semana Santa. Las iglesias principales de la ciudad incluyen la Catedral Basílica de Nuestra Señora de la Asunción, de estilo neoclásico y con una cúpula, y la Iglesia de La Ermita, del siglo XVII.";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-        child: Center(
-          child: SingleChildScrollView(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: const <Widget>[
-              Text(
-                "",
-              ),
-              Text(
-                "META CUMPLIDA PARA EL SPRINT 2",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              Image(image: AssetImage('assets/images/logo.png')),
-              SizedBox(),
-            ],
-          )),
+        body: Stack(
+      children: [
+        ListView(
+          children: [Description("Popayan", 5, msg)],
         ),
-      ),
-    );
+        Cabecera()
+      ],
+    ));
   }
 }
