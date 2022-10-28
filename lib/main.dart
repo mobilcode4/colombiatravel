eba1c97e3b50be24eebd5a2fcf00ca22f178fdf0
johnsito <jotazo@hotmail.com>
2022-10-28 10:38:46 -0500

    Fix msg login and rename class

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
-import 'LoginPage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:login/pages/splash_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+    return MaterialApp(
+      title: 'Login',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("en", "US"), Locale("es", "CO")],
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Splash_page(),
     );
   }
 }
